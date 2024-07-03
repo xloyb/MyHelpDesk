0591e88490d431a81392fb231f387f04bab46d56
mydevify <boussaadilouay9@gmail.com>
2024-07-03 12:34:54 +0100

    added sidebar

---
+++ DiscordKeeper/src/components/Sidebar.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='h-screen overflow-hidden sticky top-0 z-30 overflow-x-hidden '>
+
+    <div className="z-50 drawer lg:drawer-open">
+    <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+    <div className="drawer-content flex flex-col items-center justify-center">
+      {/* Page content here */}
+     
+    </div>
+    <div className="drawer-side">
+      <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
+      <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+        {/* Sidebar content here */}
+        <li><a>Sidebar Item 1</a></li>
+        <li><a>Sidebar Item 2</a></li>
+      </ul>
+    </div>
+  </div>
+    </div>
+  )
+}
+
+export default Sidebar
