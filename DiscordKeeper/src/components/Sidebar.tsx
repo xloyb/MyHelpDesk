690c45f623d2cee8c4706b66763184b65316f705
mydevify <boussaadilouay9@gmail.com>
2024-07-05 13:42:12 +0100

    "Removed Clerk webhook route, updated User model in Prisma schema, modified Sidebar component to use Clerk auth, and updated tsconfig.json include paths."

--- DiscordKeeper/src/components/Sidebar.tsx
+++ DiscordKeeper/src/components/Sidebar.tsx
@@ -1,6 +1,13 @@
+import { auth } from '@clerk/nextjs/server'
 import React from 'react'
 
 const Sidebar = () => {
+
+  const { userId } = auth();
+
+  console.log(userId)
+
+
   return (
     <div className='h-screen overflow-hidden sticky top-0 z-30 overflow-x-hidden '>
 
@@ -11,13 +18,17 @@ const Sidebar = () => {
      
     </div>
     <div className="drawer-side">
-      <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
-      <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
-        {/* Sidebar content here */}
-        <li><a>Sidebar Item 1</a></li>
-        <li><a>Sidebar Item 2</a></li>
-      </ul>
+  <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
+  <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4 flex flex-col justify-between">
+    {/* Sidebar content here */}
+    <div>
+      <li><a>Sidebar Item 1</a></li>
+      <li><a>Sidebar Item 2</a></li>
     </div>
+      <button className="btn btn-outline btn-success mt-auto">New Ticket</button>
+  </ul>
+</div>
+
   </div>
     </div>
   )
