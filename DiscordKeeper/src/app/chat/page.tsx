369529b7f457489e37156403885b2a91070e02f1
mydevify <boussaadilouay9@gmail.com>
2024-07-08 19:01:39 +0100

    "Refactored ChatPage component to add access control and updated imports; reformatted code in page.tsx"

--- DiscordKeeper/src/app/chat/page.tsx
+++ DiscordKeeper/src/app/chat/page.tsx
@@ -1,37 +1,26 @@
-import React from 'react'
-import styles from '@/app/main.module.css'
-import Sidebar from '@/components/Sidebar'
-import MyChat from '@/components/MyChat'
-import ChatNavbar from '@/components/ChatNavbar'
+import React from "react";
+import styles from "@/app/main.module.css";
+import Sidebar from "@/components/Sidebar";
+import MyChat from "@/components/MyChat";
+import ChatNavbar from "@/components/ChatNavbar";
 
 const Chat = () => {
   return (
-
-
-    <div>
-
-<div className={styles.container}>
+    <div className={styles.container}>
       <div className={styles.menu}>
-        <Sidebar/> 
+        <Sidebar />
       </div>
       <div className={styles.content}>
+        <div className="h-screen overflow-hidden sticky top-0  overflow-x-hidden">
+          <ChatNavbar />
 
-    <div className='h-screen overflow-hidden sticky top-0  overflow-x-hidden' >
-        <ChatNavbar/>
+          <MyChat />
 
-        <MyChat/> 
-        
-        {/* <Footer/> */}
-
-      </div>
-
+          {/* <Footer/> */}
+        </div>
       </div>
     </div>
+  );
+};
 
-
-
-    </div>
-  )
-}
-
-export default Chat
+export default Chat;
