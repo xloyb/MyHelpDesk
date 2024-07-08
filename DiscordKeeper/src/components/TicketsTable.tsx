5c877ce0c413b668638381c3ac042c835ecfacde
mydevify <boussaadilouay9@gmail.com>
2024-07-08 11:49:43 +0100

    tickets table component

---
+++ DiscordKeeper/src/components/TicketsTable.tsx
@@ -0,0 +1,52 @@
+import { Ticket } from "@prisma/client";
+
+  interface Props {
+    tickets: Ticket[];
+    handleStatusChange: (ticketId: number, status: string) => void;
+  }
+  
+  const TicketsTable: React.FC<Props> = ({ tickets = [], handleStatusChange }) => {
+    return (
+      <div>
+        <h2>Tickets</h2>
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Content</th>
+                <th>Status</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tickets.map((ticket) => (
+                <tr key={ticket.id}>
+                  <td>{ticket.id}</td>
+                  <td>{ticket.title}</td>
+                  <td>{ticket.content}</td>
+                  <td>{ticket.status}</td>
+                  <td>
+                    <select
+                      value={ticket.status || 'pending'}
+                      onChange={(e) => handleStatusChange(ticket.id, e.target.value)}
+                      className="select select-bordered"
+                    >
+                      <option value="open">Open</option>
+                      <option value="closed">Closed</option>
+                      <option value="pending">Pending</option>
+                    </select>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  };
+  
+  export default TicketsTable;
+  
