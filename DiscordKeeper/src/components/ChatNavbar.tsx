18d519afa13f24b2d09975315368ece6fc0f7392
mydevify <boussaadilouay9@gmail.com>
2024-07-10 22:25:33 +0100

    "Updated settings.json and ChatNavbar.tsx: changed sitename, added Announcement component"

--- DiscordKeeper/src/components/ChatNavbar.tsx
+++ DiscordKeeper/src/components/ChatNavbar.tsx
@@ -2,8 +2,8 @@ import { ClerkLoaded, ClerkLoading, SignedIn, SignedOut, UserButton } from '@cle
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react'
-// import { FaRegArrowAltCircleRight } from "react-icons/fa";
 import { FaCircleRight } from "react-icons/fa6";
+import Announcement from './Announcement';
 
 
 
@@ -93,6 +93,7 @@ const ChatNavbar = () => {
                 </div>
 
             </div>
+            <Announcement/>
         </div>
     )
 }
