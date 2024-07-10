0c66d6d7b528ea1bedd9f08cbac59e51c2618e5a
mydevify <boussaadilouay9@gmail.com>
2024-07-10 23:54:49 +0100

    admincp settings page

---
+++ DiscordKeeper/src/app/admincp/settings/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { useState, useEffect } from "react";
+import { isAdmin } from "@/lib/user";
+import Sidebar from "@/components/Sidebar";
+import ChatNavbar from "@/components/ChatNavbar";
+import Settings from "@/components/Settings";
+import styles from "@/app/main.module.css";
+import { useAuth } from "@clerk/nextjs";
+
+const AdminCPSettings = () => {
+  const { userId } = useAuth();
+  const [isAdminMember, setisAdminMember] = useState<boolean>(false);
+
+  useEffect(() => {
+    const checkUserRole = async () => {
+      if (userId) {
+        const isAdminMember = await isAdmin(userId);
+        setisAdminMember(isAdminMember);
+      }
+    };
+
+    checkUserRole();
+  }, [userId]);
+
+  if (!isAdminMember) {
+    return <div>You do not have access to this section.</div>;
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.menu}>
+        <Sidebar />
+      </div>
+      <div className={styles.content}>
+        <div className="h-screen overflow-hidden sticky top-0 overflow-y-auto">
+          <ChatNavbar />
+          <div className="overflow-y-auto">
+            <h1>AdminCP - Settings</h1>
+            <Settings />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AdminCPSettings;
