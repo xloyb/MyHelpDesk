48796c7e37cfec7fd62796502aaa9c1a7e7fb654
mydevify <boussaadilouay9@gmail.com>
2024-07-11 16:46:35 +0100

    created sendVouchNotification

---
+++ DiscordKeeper/utils/sendVouchNotification.ts
@@ -0,0 +1,20 @@
+import { BOT_TOKEN, DISCORD_CHANNEL_ID, DOMAIN } from "../config";
+import { sendDiscordMessage } from "./sendDiscordMessage";
+
+interface VouchNotificationParams {
+  vouchedBy: string;
+  vouchedTo: string;
+  message: string;
+}
+
+export const sendVouchNotification = async ({ vouchedBy, vouchedTo, message }: VouchNotificationParams) => {
+  const embedContent = {
+    embeds: [{
+      title: 'New Vouch Created',
+      description: `A new vouch has been created by ${vouchedBy} for ${vouchedTo}:\n\n**Message**: ${message}`,
+      color: 0x00AE86, 
+    }],
+  };
+
+  await sendDiscordMessage(DISCORD_CHANNEL_ID, embedContent, BOT_TOKEN);
+};
