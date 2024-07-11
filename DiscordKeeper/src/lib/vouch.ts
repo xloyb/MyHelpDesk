d12234090241b935262700fd9567cfd63baf901c
mydevify <boussaadilouay9@gmail.com>
2024-07-11 16:46:12 +0100

    "Removed getAllVouches function and added notification sending to createVouch function"

--- DiscordKeeper/src/lib/vouch.ts
+++ DiscordKeeper/src/lib/vouch.ts
@@ -1,25 +1,20 @@
 "use server"
 
+import { sendVouchNotification } from "../../utils/sendVouchNotification";
 import prisma from "./client";
-
-
-// Get all vouches
-export const getAllVouches = async () => {
-  return prisma.vouch.findMany({
-    include: {
-      vouchedByUser: true,
-      vouchedToUser: true,
-    },
-  });
-};
 
 // Create a vouch
 export const createVouch = async (vouchedBy: string, vouchedTo: string, message: string) => {
-  return prisma.vouch.create({
+  const createdVouch = await prisma.vouch.create({
     data: {
       vouchedBy,
       vouchedTo,
       message,
     },
   });
+
+  await sendVouchNotification({ vouchedBy, vouchedTo, message });
+
+  return createdVouch;
 };
+
