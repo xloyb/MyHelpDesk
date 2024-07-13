d48f97e7a119b3898f505545332cac0dc248966e
mydevify <boussaadilouay9@gmail.com>
2024-07-13 10:24:20 +0100

    created a navbar for the index page

---
+++ DiscordKeeper/src/components/index/IndexNav.tsx
@@ -0,0 +1,95 @@
+import { ClerkLoaded, ClerkLoading, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react'
+import { FaCircleRight } from "react-icons/fa6";
+
+
+const IndexNav = () => {
+    return (
+        <div>
+            <div className=" navbar bg-base-100">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h16M4 18h7" />
+                            </svg>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            <li><a>Homepage</a></li>
+                            <li><a>Portfolio</a></li>
+                            <li><a>About</a></li>
+                        </ul>
+                    </div>
+
+                  
+
+                </div>
+                <div className="navbar-center">
+                    <a className="btn btn-ghost text-xl">Discord Keeper</a>
+                </div>
+                <div className="navbar-end">
+                    <ClerkLoading>
+                        <span className="loading loading-ring loading-lg"></span>
+                    </ClerkLoading>
+                   <ClerkLoaded>
+
+                    <SignedIn>
+                        {/* <div className="dropdown dropdown-end">
+                            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                                <div className="w-10 rounded-full">
+                                    <Image
+                                        src="https://www.mydevify.com/icon.png"
+                                        width={500}
+                                        height={500}
+                                        alt="Picture of the author"
+                                    />
+                                    <img
+                                    alt="Tailwind CSS Navbar component"
+                                    src="https://www.mydevify.com/icon.png" />
+                                </div>
+                            </div>
+                            <ul
+                                tabIndex={0}
+                                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                                <li>
+                                    <a className="justify-between">
+                                        Profile
+                                        <span className="badge">New</span>
+                                    </a>
+                                </li>
+                                <li><a>Settings</a></li>
+                                <li><a>Logout</a></li>
+                            </ul>
+                        </div> */}
+                        <UserButton/>
+                    </SignedIn>
+
+                    <SignedOut>
+                        <div className="flex items-center gap-2 text-sm">
+                            <Image src="/img/login.png" alt="" width={20} height={20} />
+                            <Link href="/sign-in">Login/Register</Link>
+                        </div>
+                    </SignedOut>
+                    </ClerkLoaded>
+
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default IndexNav
