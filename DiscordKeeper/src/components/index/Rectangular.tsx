dbb474600fa6118bfffab2d6e9c4eff1f2023f92
mydevify <boussaadilouay9@gmail.com>
2024-07-13 10:24:41 +0100

    created a Rectangular for the index page

---
+++ DiscordKeeper/src/components/index/Rectangular.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Rectangular = () => {
+  return (
+    <>
+    <div className="mx-4 md:mx-10 lg:mx-10 flex ">
+      <div className="w-full max-w-screen-lg bg-white p-4 border border-gray-200 rounded-3xl shadow-lg">
+        <div className="prose text-left">
+          <h1 className="text-primary">Your Ticket Solution</h1>
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Rectangular
