ad12211f80d0c9d126f2fd6cf39d4f8cc96b258a
mydevify <boussaadilouay9@gmail.com>
2024-07-13 10:24:59 +0100

    created a static Services for the index page

---
+++ DiscordKeeper/src/components/index/Services.tsx
@@ -0,0 +1,119 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Services = () => {
+  return (
+    <>
+        <div
+        className={`content-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 "`}
+      >
+        <div
+      className="mt-6 card card-compact bg-base-100 shadow-xl w-auto m-2" 
+    >
+      <figure>
+        <img src="https://www.mydevify.com/assets/index.494ac568.png" alt="test" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">title</h2>
+        <p>description</p>
+        <div className="card-actions justify-end">
+          <Link href={`/dashboard/myrequests/test`}>
+            <button className="btn btn-primary">Buy</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+
+    <div
+      className="mt-6 card card-compact bg-base-100 shadow-xl w-auto m-2" 
+    >
+      <figure>
+        <img src="https://www.mydevify.com/assets/index.494ac568.png" alt="test" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">title</h2>
+        <p>description</p>
+        <div className="card-actions justify-end">
+          <Link href={`/dashboard/myrequests/test`}>
+            <button className="btn btn-primary">Buy</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+
+
+
+
+    <div
+      className="mt-6 card card-compact bg-base-100 shadow-xl w-auto m-2" 
+    >
+      <figure>
+        <img src="https://www.mydevify.com/assets/index.494ac568.png" alt="test" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">title</h2>
+        <p>description</p>
+        <div className="card-actions justify-end">
+          <Link href={`/dashboard/myrequests/test`}>
+            <button className="btn btn-primary">Buy</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+
+
+
+
+    <div
+      className="mt-6 card card-compact bg-base-100 shadow-xl w-auto m-2" 
+    >
+      <figure>
+        <img src="https://www.mydevify.com/assets/index.494ac568.png" alt="test" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">title</h2>
+        <p>description</p>
+        <div className="card-actions justify-end">
+          <Link href={`/dashboard/myrequests/test`}>
+            <button className="btn btn-primary">Buy</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+
+
+
+
+    <div
+      className="mt-6 card card-compact bg-base-100 shadow-xl w-auto m-2" 
+    >
+      <figure>
+        <img src="https://www.mydevify.com/assets/index.494ac568.png" alt="test" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">title</h2>
+        <p>description</p>
+        <div className="card-actions justify-end">
+          <Link href={`/dashboard/myrequests/test`}>
+            <button className="btn btn-primary">Buy</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+
+
+
+    
+      </div>
+
+
+    </>
+  )
+}
+
+export default Services
