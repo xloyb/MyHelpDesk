38b58a83d90f6672016c74d9a50e4442b0d00d29
mydevify <boussaadilouay9@gmail.com>
2024-07-16 01:53:24 +0100

    Remove unnecessary imports of IoLockClosedOutline and FaHeartCirclePlus icons from Chat.tsx component.

--- DiscordKeeper/src/components/Chat.tsx
+++ DiscordKeeper/src/components/Chat.tsx
@@ -4,8 +4,6 @@ import { fetchCommentsByTicketToken } from '@/lib/data';
 import { useAuth } from '@clerk/nextjs';
 import Image from 'next/image';
 import { CiSettings } from "react-icons/ci";
-import { IoLockClosedOutline } from "react-icons/io5";
-import { FaHeartCirclePlus } from "react-icons/fa6";
 import VouchModal from './VouchModal';
 import TicketStatusModal from './TicketStatusModal';
 
