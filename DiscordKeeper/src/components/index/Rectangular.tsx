baa319eefa7f3b51a8b116f6574d0cc8e8239abd
mydevify <boussaadilouay9@gmail.com>
2024-07-16 22:44:44 +0100

    "Updated Rectangular component: changed container div styles, removed 'prose' class, and updated text styles."

--- DiscordKeeper/src/components/index/Rectangular.tsx
+++ DiscordKeeper/src/components/index/Rectangular.tsx
@@ -3,12 +3,11 @@ import React from 'react'
 const Rectangular = () => {
   return (
     <>
-   <div className="flex mt-4">
-  <div className="w-full max-w-7xl bg-base-150 p-6 rounded-3xl shadow-lg mx-4 flex items-center">
-    <div className="prose text-left w-full">
-      <h1 className="font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl prose prose-sm md:prose-base w-full flex-grow pt-10">Beyond Discord: Secure and private tickets.</h1>
-      <p className="text-gray-600 mt-2 text-sm sm:text-base md:text-lg lg:text-xl">
-      Confidential ticketing made easy: Open and manage tickets while safeguarding your chat privacy from Discord.
+   <div className="flex mt-4 mb-4">
+  <div className="w-full max-w-7xl bg-white p-6 rounded-3xl shadow-lg mx-4 flex items-center">
+    <div className="text-left w-full">
+      <h1 className="dark:text-base-300 text-primary text-3xl sm:text-3xl md:text-4xl lg:text-5xl prose prose-sm md:prose-base w-full flex-grow font-black	">Beyond Discord: Secure and private tickets.</h1>
+      <p className="dark:text-slate-300 text-primary mt-2 text-sm sm:text-base md:text-lg lg:text-xl">
       </p>
     </div>
   </div>
