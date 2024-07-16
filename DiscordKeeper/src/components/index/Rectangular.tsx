7e88643233aa9006484494b467b485db5e083d91
mydevify <boussaadilouay9@gmail.com>
2024-07-16 03:41:36 +0100

    "Update Rectangular component: modify container div classes, update h1 and p text content"

--- DiscordKeeper/src/components/index/Rectangular.tsx
+++ DiscordKeeper/src/components/index/Rectangular.tsx
@@ -3,12 +3,12 @@ import React from 'react'
 const Rectangular = () => {
   return (
     <>
-   <div className="flex justify-center mt-4">
-  <div className="w-full max-w-7xl bg-white p-6 border border-gray-200 rounded-3xl shadow-lg mx-4 flex items-center">
+   <div className="flex mt-4">
+  <div className="w-full max-w-7xl bg-base-150 p-6 rounded-3xl shadow-lg mx-4 flex items-center">
     <div className="prose text-left w-full">
-      <h1 className="font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl">Your Ticket Solution</h1>
+      <h1 className="font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl prose prose-sm md:prose-base w-full flex-grow pt-10">Beyond Discord: Secure and private tickets.</h1>
       <p className="text-gray-600 mt-2 text-sm sm:text-base md:text-lg lg:text-xl">
-        Manage your tickets effortlessly with our intuitive platform.
+      Confidential ticketing made easy: Open and manage tickets while safeguarding your chat privacy from Discord.
       </p>
     </div>
   </div>
