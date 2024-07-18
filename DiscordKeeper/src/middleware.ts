88d031a98c1c2b95dcb99eba50ac6bd8f1866374
mydevify <boussaadilouay9@gmail.com>
2024-07-19 00:43:30 +0100

    "Updated protected route matcher to include /chat, /modcp, and /admincp patterns"

--- DiscordKeeper/src/middleware.ts
+++ DiscordKeeper/src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/test(.*)"]);
+const isProtectedRoute = createRouteMatcher(["/chat(.*)","/modcp(.*)","/admincp(.*)"]);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
