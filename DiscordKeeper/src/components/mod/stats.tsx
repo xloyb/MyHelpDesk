d2a5e9df4fcb4448a95cb9f6285453c6dc5306d3
mydevify <boussaadilouay9@gmail.com>
2024-07-18 02:26:31 +0100

    "Refactor StatsComponent to display stats in a grid layout"

--- DiscordKeeper/src/components/mod/stats.tsx
+++ DiscordKeeper/src/components/mod/stats.tsx
@@ -1,36 +1,39 @@
-import React from 'react'
-import { FaUser } from 'react-icons/fa6'
+import React from 'react';
+import { FaUser } from 'react-icons/fa6';
 
 const StatsComponent = () => {
   return (
-    <>
-    <div className="stats bg-base-100 shadow">
-  
-  <div className="stat">
-    <div className="stat-figure invisible md:visible">
-        <FaUser className='w-8 h-8'/>
+    <div className="bg-base-200 card mx-6 mt-5 md:pt-4 px-6">
+      <div className="content-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+        <div className="stat flex justify-between items-center bg-neutral text-neutral-content shadow-lg p-4 rounded-lg">
+          <div>
+            <div className="stat-title text-sm">Total Users</div>
+            <div className="stat-value text-2xl lg:text-3xl">$34,545</div>
+            <div className="stat-desc text-xs">Current month</div>
+          </div>
+          <FaUser className="w-8 h-8 text-gray-400" />
+        </div>
+    
+        <div className="stat flex justify-between items-center bg-neutral text-neutral-content shadow-lg p-4 rounded-lg">
+          <div>
+            <div className="stat-title text-sm">Total Tickets</div>
+            <div className="stat-value text-2xl lg:text-3xl">x</div>
+            <div className="stat-desc text-xs">Current month</div>
+          </div>
+          <FaUser className="w-8 h-8 text-gray-400" />
+        </div>
+    
+        <div className="stat flex justify-between items-center bg-neutral text-neutral-content shadow-lg p-4 rounded-lg">
+          <div>
+            <div className="stat-title text-sm">Pending Tickets</div>
+            <div className="stat-value text-2xl lg:text-3xl">x</div>
+            <div className="stat-desc text-xs">Current month</div>
+          </div>
+          <FaUser className="w-8 h-8 text-gray-400" />
+        </div>
+      </div>
     </div>
-    <div className="stat-title">Total Users</div>
-    <div className="stat-value">12</div>
-  </div>
-  
-  <div className="stat">
-    <div className="stat-figure invisible md:visible">
-        <FaUser className='w-8 h-8'/>
-    </div>
-    <div className="stat-title">Total Tickets</div>
-    <div className="stat-value">x</div>
-  </div>
+  );
+};
 
-  <div className="stat">
-    <div className="stat-figure invisible md:visible">
-        <FaUser className='w-8 h-8'/>
-    </div>
-    <div className="stat-title">Pending Tickets</div>
-    <div className="stat-value">x</div>
-  </div>
-</div></>
-  )
-}
-
-export default StatsComponent
+export default StatsComponent;
