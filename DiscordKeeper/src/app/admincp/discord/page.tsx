cd61b85d4d0ae5a0e9cfdcd5509c217a61409b9d
mydevify <boussaadilouay9@gmail.com>
2024-07-19 21:27:36 +0100

    "Refactor AdminCP_Discord component to add role management, authentication, and conditional rendering"

--- DiscordKeeper/src/app/admincp/discord/page.tsx
+++ DiscordKeeper/src/app/admincp/discord/page.tsx
@@ -1,12 +1,63 @@
-import DiscordInfoPage from '@/components/admincp/Discord_info'
-import React from 'react'
+"use client";
+import { useState, useEffect } from "react";
+import { getAllRoles } from "@/lib/role";
+import { isAdmin } from "@/lib/user";
+import Sidebar from "@/components/Sidebar";
+import ChatNavbar from "@/components/ChatNavbar";
+import styles from "@/app/main.module.css";
+import { useAuth } from "@clerk/nextjs";
+import ManageRoles from "@/components/RolesTable";
+import DiscordInfoPage from "@/components/admincp/Discord_info";
 
 const AdminCP_Discord = () => {
-  return (
-    <>
-    <DiscordInfoPage/>
-    </>
-  )
-}
+  const { userId } = useAuth();
+  const [roles, setRoles] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [isAdminMember, setisAdminMember] = useState<boolean>(false);
 
-export default AdminCP_Discord
+  useEffect(() => {
+    const fetchData = async () => {
+      const roles = await getAllRoles();
+      setRoles(roles);
+      setLoading(false);
+    };
+
+    const checkUserRole = async () => {
+      if (userId) {
+        const isAdminMember = await isAdmin(userId);
+        setisAdminMember(isAdminMember);
+      }
+    };
+
+    fetchData();
+    checkUserRole();
+  }, [userId]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.menu}>
+        <Sidebar />
+      </div>
+      <div className={styles.content}>
+        <div className="h-screen bg-base-200 overflow-hidden sticky top-0 overflow-y-auto">
+          <ChatNavbar />
+          <div className="overflow-y-auto">
+            {isAdminMember ? (
+                <>
+                <DiscordInfoPage/>
+                </>
+            ) : (
+              <p>You do not have access to this section.</p>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AdminCP_Discord;
