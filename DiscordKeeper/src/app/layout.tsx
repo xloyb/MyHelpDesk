730e2a1708d9122858ad4a4c46e4087b3ac4fd3f
mydevify <boussaadilouay9@gmail.com>
2024-07-19 20:48:39 +0100

    Remove Footer import from layout.tsx

--- DiscordKeeper/src/app/layout.tsx
+++ DiscordKeeper/src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
