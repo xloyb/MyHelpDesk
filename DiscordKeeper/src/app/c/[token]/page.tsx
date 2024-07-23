b0b1b4d3553ea3d9f96f2ed6fdaeeb07e7bf3576
mydevify <boussaadilouay9@gmail.com>
2024-07-23 05:55:37 +0100

    created verifyToken

--- DiscordKeeper/src/app/c/[token]/page.tsx
+++ DiscordKeeper/src/app/c/[token]/page.tsx
@@ -4,15 +4,34 @@ import RealTimeChat from '@/components/WebSocketChat/MyRealTimeChat';
 import ChatNavbar from '@/components/ChatNavbar';
 import Sidebar from '@/components/Sidebar';
 import styles from '@/app/main.module.css';
+import { checkTokenExists } from '@/lib/actions';
+import { useState } from 'react';
 
 
 const Chat = dynamic(() => import('@/components/Chat'), { ssr: false });
 
 const ChatPage = () => {
+    const [isValidToken, setIsValidToken] = useState<boolean | null>(null);
 
   const path = window.location.pathname;
   const tokens = path.split('/').filter(Boolean);
   const token = tokens.length > 1 ? tokens[1] : '';
+
+
+  const verifyToken = async () => {
+    try {
+      if (token) {
+        const tokenExists = await checkTokenExists(token);
+        setIsValidToken(tokenExists);
+      } else {
+        setIsValidToken(false);
+      }
+    } catch (error) {
+      console.error('Error verifying token:', error);
+      setIsValidToken(false);
+    }
+  };
+
 
   const  ticketId  = "4";
 
