b7cb046b74a37bc33baf1b7a70919b02caea052a
mydevify <boussaadilouay9@gmail.com>
2024-07-25 12:25:45 +0100

    created page.tsx

---
+++ DiscordKeeper/src/app/crypto/page.tsx
@@ -0,0 +1,13 @@
+
+import CryptoTracker from '@/components/CryptoList';
+import React from 'react';
+
+const Page: React.FC = () => {
+  return (
+    <div>
+      <CryptoTracker/>
+    </div>
+  );
+};
+
+export default Page;
