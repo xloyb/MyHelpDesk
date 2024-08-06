b44091a4ce386556ea0e3739fe1fe78ecfeb93ca
mydevify <boussaadilouay9@gmail.com>
2024-08-06 11:58:23 +0100

    created StaffTicketNote table

--- DiscordKeeper/public/DELETE_AFTER_SETUP/insert_roles_data.sql
+++ DiscordKeeper/public/DELETE_AFTER_SETUP/insert_roles_data.sql
@@ -4,3 +4,9 @@ INSERT INTO Role (name, description) VALUES ('Client', 'Client role, regular use
 INSERT INTO Role (name, description) VALUES ('Support', 'Support role, support only, NO DEALS');
 INSERT INTO Role (name, description) VALUES ('Mod', 'Moderator role, moderation duties');
 INSERT INTO Role (name, description) VALUES ('Admin', 'Administrator role, full access');
+
+INSERT INTO user (id, email, avatar, cover, name, userid, roleId) VALUES
+('user_2kHN1lJ3j7KO2jDsVksym31QhoG', 'contactxloy@gmail.com', 'https://images.clerk.dev/oauth_google/img_2kHN1lJ3j7KO2jDsVksym31QhoG', '/img/noCover.png', 'xloy', NULL, 4);
+
+INSERT INTO user (id, email, avatar, cover, name, userid, roleId) VALUES 
+('user_2kHNEANVluU3lEr7KsHABBTrwMH', 'louayeverywhere@gmail.com', 'https://www.gravatar.com/avatar?d=mp', '/img/noCover.png', 'louay', NULL, 1);
