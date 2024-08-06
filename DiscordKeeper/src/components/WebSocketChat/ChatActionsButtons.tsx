85b708870a95cb05c57aaac3382b0e9ea48e73bb
mydevify <boussaadilouay9@gmail.com>
2024-08-06 20:03:06 +0100

    fixed ChatActionButtons

--- DiscordKeeper/src/components/WebSocketChat/ChatActionsButtons.tsx
+++ DiscordKeeper/src/components/WebSocketChat/ChatActionsButtons.tsx
@@ -1,59 +1,40 @@
-// // src/components/ActionButtons.tsx
-// import React, { useEffect, useState } from 'react';
-// import { CanBan } from '@/lib/ban';
-// import BanModal from '../mod/BanModal';
-// import CopyUrlComponent from '../ShareTicketLink';
-// import DownloadChatComponent from '../DownloadChat';
-// import VouchModal from '../VouchModal';
-// import TicketStatusModal from '../TicketStatusModal';
-
-// interface ActionButtonsProps {
-//   token: string;
-//   userId: string;
-// }
-
-// const ActionButtons: React.FC<ActionButtonsProps> = ({ token, userId }) => {
-//   const [showBanModal, setShowBanModal] = useState(false);
-
-//   useEffect(() => {
-//     const checkBanPermission = async () => {
-//       if (userId) {
-//         const canBan = await CanBan(userId);
-//         setShowBanModal(canBan);
-//       }
-//     };
-
-//     checkBanPermission();
-//   }, [userId]);
-
-//   return (
-//     <div className="flex flex-wrap gap-2">
-//       {showBanModal && <BanModal token={token} />}
-//       <CopyUrlComponent />
-//       <DownloadChatComponent token={token} />
-//       <VouchModal />
-//       <TicketStatusModal token={token} />
-//     </div>
-//   );
-// };
-
-// export default ActionButtons;
-
-
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { CanBan } from '@/lib/ban';
+import BanModal from '../mod/BanModal';
+import CopyUrlComponent from '../ShareTicketLink';
+import DownloadChatComponent from '../DownloadChat';
+import VouchModal from '../VouchModal';
+import TicketStatusModal from '../TicketStatusModal';
 
 interface ActionButtonsProps {
   token: string;
   userId: string | null | undefined;
 }
 const ChatActionButtons: React.FC<ActionButtonsProps> = ({ token, userId }) => {
+  const [showBanModal, setShowBanModal] = useState(false);
+
+    useEffect(() => {
+      const checkBanPermission = async () => {
+        if (userId) {
+          const canBan = await CanBan(userId);
+          setShowBanModal(canBan);
+        }
+      };
+  
+      checkBanPermission();
+    }, [userId]);
+
   return (
     <div className="container mx-auto p-4">
       <div className="dropdown dropdown-top dropdown-end">
         <div tabIndex={0} role="button" className="btn m-1">Click</div>
-        <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
-          <li><a>Item 1</a></li>
-          <li><a>Item 2</a></li>
+        <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box w-52 p-2 shadow z-30">
+        {showBanModal && <BanModal token={token} />}
+      <CopyUrlComponent />
+     <DownloadChatComponent token={token} />
+       <VouchModal />
+     <TicketStatusModal token={token} />
+     {/* <li><a>Item 1</a></li> */}
         </ul>
       </div>
     </div>
