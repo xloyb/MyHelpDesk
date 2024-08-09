fc2fd86d4a6c66cfd5053cc5969d0198244f526c
mydevify <boussaadilouay9@gmail.com>
2024-08-09 12:03:22 +0100

    added arrows

--- DiscordKeeper/src/components/SideBarTickets.tsx
+++ DiscordKeeper/src/components/SideBarTickets.tsx
@@ -42,41 +42,49 @@ import React, { useState } from 'react';
 import { Ticket } from '@prisma/client';
 import { MdOutlineSupportAgent } from 'react-icons/md';
 import Link from 'next/link';
+import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io';
 
 interface ShowMoreTicketsListProps {
-  tickets: Ticket[];
+    tickets: Ticket[];
 }
 
 const ShowMoreTicketsList: React.FC<ShowMoreTicketsListProps> = ({ tickets }) => {
-  const [showMore, setShowMore] = useState(false);
+    const [showMore, setShowMore] = useState(false);
 
-  const handleToggle = () => {
-    setShowMore(!showMore);
-  };
+    const handleToggle = () => {
+        setShowMore(!showMore);
+    };
 
-  return (
-    <div className="bg-base-100 card p-6 mx-auto mt-5 w-full max-w-lg">
-      <h2 className="text-xl font-semibold mb-4">Tickets List</h2>
-      <ul className="menu rounded-box w-56">
-        {tickets.slice(0, 5).map(ticket => (
-            <Link key={ticket.id} href={`/c/${ticket.token}`}>
-          <li> <a><MdOutlineSupportAgent />  {ticket.title}</a></li>
-          </Link>
-        ))}
-        {showMore && tickets.slice(5).map(ticket => (
-            <Link key={ticket.id} href={`/c/${ticket.token}`}>
-          <li> <a><MdOutlineSupportAgent />  {ticket.title}</a></li>
-          </Link>
+    return (
+        <div className="bg-base-100 card p-6 mx-auto mt-5 w-full max-w-lg">
+            <h2 className="text-xl font-semibold mb-4">Tickets List</h2>
+            <ul className="menu rounded-box w-56">
+                {tickets.slice(0, 5).map(ticket => (
+                    <Link key={ticket.id} href={`/c/${ticket.token}`}>
+                        <li> <a><MdOutlineSupportAgent />  {ticket.title}</a></li>
+                    </Link>
                 ))}
-      </ul>
-      <button
-        onClick={handleToggle}
-        className={`btn ${showMore ? 'btn-secondary' : 'btn-primary'} mt-4`}
-      >
-        {showMore ? 'Show Less' : 'Show More'}
-      </button>
-    </div>
-  );
+                {showMore && tickets.slice(5).map(ticket => (
+                    <Link key={ticket.id} href={`/c/${ticket.token}`}>
+                        <li> <a><MdOutlineSupportAgent />  {ticket.title}</a></li>
+                    </Link>
+                ))}
+            </ul>
+            <button
+                onClick={handleToggle}
+                className={`btn mt-auto btn-xs sm:btn-sm md:btn-md`}
+            >
+                {showMore ? (
+                    <>
+                        Show Less <IoIosArrowUp className="ml-2" />
+                    </>
+                ) : (
+                    <>
+                        Show More <IoIosArrowDown className="ml-2" />
+                    </>
+                )}      </button>
+        </div>
+    );
 };
 
 export default ShowMoreTicketsList;
