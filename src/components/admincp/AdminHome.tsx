72d21c4339525b6b41ceb8db042fe182b3f70487
mydevify <boussaadilouay9@gmail.com>
2024-08-11 14:04:56 +0100

    created admin home

---
+++ src/components/admincp/AdminHome.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AdminHome = () => {
+    return (
+        <div className="bg-base-100 card mx-6 mt-5 md:pt-4 px-6">
+            <div className="text-xl font-semibold inline-block">Admin Controle Panel</div>
+            <div className="divider mt-2"></div>
+            <div className="content-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+            </div>
+
+        </div>
+    )
+}
+
+export default AdminHome
