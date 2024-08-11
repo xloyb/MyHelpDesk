2bee7dbe86002d0d52ca5c629251d8d14f80bfaa
mydevify <boussaadilouay9@gmail.com>
2024-08-11 10:21:00 +0100

    created store route in admincp to manage services

---
+++ src/app/admincp/store/page.tsx
@@ -0,0 +1,61 @@
+"use client"
+import { useState, useEffect } from "react";
+import { getAllRoles } from "@/lib/role";
+import { isAdmin } from "@/lib/user";
+import Sidebar from "@/components/Sidebar";
+import ChatNavbar from "@/components/ChatNavbar";
+import styles from "@/app/main.module.css";
+import { useAuth } from "@clerk/nextjs";
+import ManageRoles from "@/components/RolesTable";
+
+const AdminCPRoles = () => {
+  const { userId } = useAuth();
+  const [roles, setRoles] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [isAdminMember, setisAdminMember] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(false);
+    };
+
+    const checkUserRole = async () => {
+      if (userId) {
+        const isAdminMember = await isAdmin(userId);
+        setisAdminMember(isAdminMember);
+      }
+    };
+
+    fetchData();
+    checkUserRole();
+  }, [userId]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.menu}>
+        <Sidebar />
+      </div>
+      <div className={styles.content}>
+        <div className="h-screen bg-base-200 overflow-hidden sticky top-0 overflow-y-auto">
+          <ChatNavbar />
+          <div className="overflow-y-auto">
+            {isAdminMember ? (
+                <>
+
+
+                </>
+            ) : (
+              <p>You do not have access to this section.</p>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AdminCPRoles;
