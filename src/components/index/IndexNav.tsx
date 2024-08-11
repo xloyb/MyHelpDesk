558dcf48596acf21b40f2c9f70a5850a9b1d1d3b
mydevify <boussaadilouay9@gmail.com>
2024-08-11 15:01:04 +0100

    updated index nav

--- src/components/index/IndexNav.tsx
+++ src/components/index/IndexNav.tsx
@@ -55,7 +55,6 @@ const IndexNav = () => {
                             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
                             <li><a href='/'>Home</a></li>
                             <li><a href='/c'>Dashboard</a></li>
-                            <li><a>About</a></li>
                         </ul>
                     </div>
 
