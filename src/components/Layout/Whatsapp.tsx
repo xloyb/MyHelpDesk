40744242baaaf798e8fdaaaf45596b0f6f816c2a
mydevify <boussaadilouay9@gmail.com>
2024-08-12 17:16:37 +0100

    created whatsapp component

---
+++ src/components/Layout/Whatsapp.tsx
@@ -0,0 +1,15 @@
+import { FaWhatsapp } from "react-icons/fa";
+import React from 'react'
+
+const Whatsapp = () => {
+  return (
+
+    <div className="fixed bottom-4 right-4">
+      <a href="https://wa.me/+21650792753" target="_blank" className="btn btn-circle btn-success shadow-lg">
+      <FaWhatsapp />
+      </a>
+    </div>
+      )
+}
+
+export default Whatsapp
