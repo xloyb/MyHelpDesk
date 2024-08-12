e695932bfd9f0173de57a36942afb78a24df7a3c
mydevify <boussaadilouay9@gmail.com>
2024-08-12 22:32:54 +0100

    updated route

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,3 +1,31 @@
+// import { NextResponse } from "next/server";
+// import path from "path";
+// import { writeFile } from "fs/promises";
+
+// export const POST = async (req: Request) => {
+//   try {
+//     const formData = await req.formData();
+    
+//     const file = formData.get("file") as File;
+//     if (!file) {
+//       return NextResponse.json({ error: "No files received." }, { status: 400 });
+//     }
+    
+//     const buffer = Buffer.from(await file.arrayBuffer());
+//     const filename = Date.now() + file.name.replaceAll(" ", "_");
+    
+//     await writeFile(
+//       path.join(process.cwd(), "public/uploads/" + filename),
+//       buffer
+//     );
+    
+//     return NextResponse.json({ message: "Success", status: 201 });
+//   } catch (error) {
+//     console.log("Error occurred", error);
+//     return NextResponse.json({ message: "Failed", status: 500 });
+//   }
+// };
+
 import { NextResponse } from "next/server";
 import path from "path";
 import { writeFile } from "fs/promises";
@@ -19,7 +47,8 @@ export const POST = async (req: Request) => {
       buffer
     );
     
-    return NextResponse.json({ message: "Success", status: 201 });
+    // Return the filename along with the success message
+    return NextResponse.json({ message: "Success", filename: filename, status: 201 });
   } catch (error) {
     console.log("Error occurred", error);
     return NextResponse.json({ message: "Failed", status: 500 });
