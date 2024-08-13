410e38ff04e8c6dc0533e82b8482ee3071121525
mydevify <boussaadilouay9@gmail.com>
2024-08-13 23:22:33 +0100

    updated db

--- prisma/migrations/20240813222153_add_countries_table/migration.sql
+++ prisma/migrations/20240813222153_add_countries_table/migration.sql
@@ -126,6 +126,7 @@ CREATE TABLE `Service` (
     `categoryId` INTEGER NOT NULL,
     `amount` INTEGER NOT NULL,
     `buyOrSellType` VARCHAR(191) NOT NULL,
+    `ShoppyCode` VARCHAR(191) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
