12f73c631618208ed0622451e9eb85f3cf4b9191
mydevify <boussaadilouay9@gmail.com>
2024-08-13 12:27:04 +0100

    created countries route

---
+++ src/app/api/countries/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server';
+import { getCountries } from '@/lib/country';
+
+export async function GET(request: Request) {
+  const url = new URL(request.url);
+  const page = Number(url.searchParams.get('page')) || 1;
+  const search = url.searchParams.get('search') || '';
+
+  const skip = (page - 1) * 10;
+  const take = 10;
+
+  try {
+    const countries = await getCountries(skip, take, search);
+    return NextResponse.json(countries);
+  } catch (error) {
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+  }
+}
