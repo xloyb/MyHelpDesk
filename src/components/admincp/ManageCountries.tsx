bfa3f96f1a5ccccc8acda5243cba4fa48860a7ee
mydevify <boussaadilouay9@gmail.com>
2024-08-13 12:56:07 +0100

    updated comp

--- src/components/admincp/ManageCountries.tsx
+++ src/components/admincp/ManageCountries.tsx
@@ -59,62 +59,58 @@ const CountryTable = () => {
   };
 
   return (
-    <div className="overflow-x-auto">
-      <input
-        type="text"
-        placeholder="Search for a country..."
-        value={search}
-        onChange={handleSearch}
-        className="input input-bordered mb-4"
-      />
-      <table className="table">
-        <thead>
-          <tr>
-            <th>
-              <label>
-                <input type="checkbox" className="checkbox" />
-              </label>
-            </th>
-            <th>Name</th>
-            <th>Short Name</th>
-            <th>Enabled</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredCountries.map((country) => (
-            <tr key={country.id}>
+    <div className="bg-base-100 card mx-6 mt-5 md:pt-4 px-6">
+      <div className="text-xl font-semibold inline-block">Authorized Countries</div>
+      <div className='stat-desc text-xs'>Some countries have restrictions on cryptocurrency transactions. From here, you can disable crypto payments for a service and keep only the other payment methods available.</div>
+      <div className="divider mt-2"></div>
+      <div className="overflow-x-auto">
+        <input
+          type="text"
+          placeholder="Search for a country..."
+          value={search}
+          onChange={handleSearch}
+          className="input input-bordered mb-4"
+        />
+        <table className="table">
+          <thead>
+            <tr>
               <th>
                 <label>
-                  <input
-                    type="checkbox"
-                    className="checkbox"
-                    checked={country.enabled}
-                    onChange={() => toggleEnabled(country.id, country.enabled)}
-                  />
+                  <input type="checkbox" className="checkbox" />
                 </label>
               </th>
-              <td>{country.name}</td>
-              <td>{country.shortname}</td>
-              <td>{country.enabled ? 'Enabled' : 'Disabled'}</td>
-              <th>
-                <button className="btn btn-ghost btn-xs" onClick={() => toggleEnabled(country.id, country.enabled)}>
-                  Toggle Enabled
-                </button>
-              </th>
+              <th>Name</th>
+              <th>Short Name</th>
+              <th>Enabled</th>
+              <th></th>
             </tr>
-          ))}
-        </tbody>
-        <tfoot>
-          <tr>
-            <th></th>
-            <th>Name</th>
-            <th>Short Name</th>
-            <th>Enabled</th>
-            <th></th>
-          </tr>
-        </tfoot>
-      </table>
+          </thead>
+          <tbody>
+            {filteredCountries.map((country) => (
+              <tr key={country.id}>
+                <th>
+                  <label>
+                    <input
+                      type="checkbox"
+                      className="checkbox"
+                      checked={country.enabled}
+                      onChange={() => toggleEnabled(country.id, country.enabled)}
+                    />
+                  </label>
+                </th>
+                <td>{country.name}</td>
+                <td>{country.shortname}</td>
+                <td>{country.enabled ? 'Enabled' : 'Disabled'}</td>
+                <th>
+                  <button className="btn btn-ghost btn-xs" onClick={() => toggleEnabled(country.id, country.enabled)}>
+                    Toggle Enabled
+                  </button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
