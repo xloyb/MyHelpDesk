ec5be0301883b8d60759e6bf1a6b9bbbf90e5535
mydevify <boussaadilouay9@gmail.com>
2024-08-13 11:33:16 +0100

    added shoppy script

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -3,14 +3,16 @@ import styles from "@/app/main.module.css";
 import Footer from "@/components/Footer";
 import IndexNav from "@/components/index/IndexNav";
 import Services from "@/components/index/Services";
+import Script from "next/script"; // Import the Script component
 
 const Store = () => {
   return (
     <div className='h-screen'>
+      <Script src="https://shoppy.gg/api/embed.js" strategy="lazyOnload" />
       <div className="bg-base-200">
         <IndexNav />
         <Services />
-        <Footer/>
+        <Footer />
       </div>
     </div>
   );
