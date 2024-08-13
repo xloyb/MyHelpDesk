114b3e109c544d660bbf458b6a57a468608c950c
mydevify <boussaadilouay9@gmail.com>
2024-08-13 12:54:14 +0100

    updated route

--- src/app/api/countries/route.ts
+++ src/app/api/countries/route.ts
@@ -2,15 +2,8 @@ import { NextResponse } from 'next/server';
 import { getCountries } from '@/lib/country';
 
 export async function GET(request: Request) {
-  const url = new URL(request.url);
-  const page = Number(url.searchParams.get('page')) || 1;
-  const search = url.searchParams.get('search') || '';
-
-  const skip = (page - 1) * 10;
-  const take = 10;
-
   try {
-    const countries = await getCountries(skip, take, search);
+    const countries = await getCountries();
     return NextResponse.json(countries);
   } catch (error) {
     console.error('Error fetching countries:', error);
