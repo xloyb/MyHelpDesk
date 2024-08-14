1711ca6401a9e46728c657ba611e8bf799c86593
mydevify <boussaadilouay9@gmail.com>
2024-08-14 11:15:57 +0100

    added Shoppy buy button

--- src/components/index/ServiceTabsWithCountryCheck.tsx
+++ src/components/index/ServiceTabsWithCountryCheck.tsx
@@ -1,8 +1,8 @@
 "use client";
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import Image from 'next/image';
-import Link from 'next/link';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Image from "next/image";
+import Link from "next/link";
 
 interface Category {
   id: number;
@@ -23,17 +23,18 @@ interface Service {
 }
 
 async function fetchCountry() {
-  const response = await fetch('/api/getCountry');
+  const response = await fetch("/api/getCountry");
   const data = await response.json();
-  console.log("Get Country Logged Data:",data)
+  console.log("Get Country Logged Data:", data);
   return data.country_code;
 }
 
 async function checkCountryEnabled(countryCode: string) {
-  const response = await fetch(`/api/countries/checkcountrystatus?country_code=${countryCode}`);
+  const response = await fetch(
+    `/api/countries/checkcountrystatus?country_code=${countryCode}`
+  );
   const data = await response.json();
-  console.log("checkcountrystatus Logged Data:",data)
-
+  console.log("checkcountrystatus Logged Data:", data);
   return data.isEnabled;
 }
 
@@ -51,21 +52,21 @@ function ServiceTabsWithCountryCheck() {
         const isEnabled = await checkCountryEnabled(countryCode);
         setCountryStatus(isEnabled);
       } catch (error) {
-        console.error('Failed to fetch country status:', error);
-        setCountryStatus(null); 
+        console.error("Failed to fetch country status:", error);
+        setCountryStatus(null);
       } finally {
         setLoading(false);
       }
 
       try {
-        const categoriesResponse = await axios.get('/api/categories');
+        const categoriesResponse = await axios.get("/api/categories");
         setCategories(categoriesResponse.data);
         setActiveCategory(categoriesResponse.data[0]?.id || null);
 
-        const servicesResponse = await axios.get('/api/services');
+        const servicesResponse = await axios.get("/api/services");
         setServices(servicesResponse.data);
       } catch (error) {
-        console.error('Error fetching categories or services:', error);
+        console.error("Error fetching categories or services:", error);
       }
     }
 
@@ -73,38 +74,63 @@ function ServiceTabsWithCountryCheck() {
   }, []);
 
   const renderServicesForCategory = (categoryId: number) => {
-    return services.filter(service => service.categoryId === categoryId).map(service => (
-      <div key={service.id} className="mt-6 card card-compact shadow-xl w-auto m-2 bg-base-100">
-        <figure>
-          <div style={{ width: '100%', position: 'relative', paddingBottom: '56.25%' }}>
-            <Image
-              src={`/uploads/${service.image}`}
-              alt={service.title}
-              layout="fill"
-              objectFit="contain"
-            />
-          </div>
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">{service.title}</h2>
-          <p>{service.buyOrSellType}</p>
-          <p>
+    return services
+      .filter((service) => service.categoryId === categoryId)
+      .map((service) => (
+        <div
+          key={service.id}
+          className="mt-6 card card-compact shadow-xl w-auto m-2 bg-base-100"
+        >
+          <figure>
+            <div
+              style={{
+                width: "100%",
+                position: "relative",
+                paddingBottom: "56.25%",
+              }}
+            >
+              <Image
+                src={`/uploads/${service.image}`}
+                alt={service.title}
+                layout="fill"
+                objectFit="contain"
+              />
+            </div>
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">{service.title}</h2>
+            <p>{service.buyOrSellType}</p>
+            {/* <p>
             {countryStatus === null || countryStatus === undefined
               ? service.ShoppyCode
               : countryStatus
               ? service.ShoppyCode
               : service.ShoppyCodeNoCrypto}
-          </p>
+          </p> */}
 
-          <div className="card-actions justify-end">
-            <p className="text-lg font-bold leading-10">${service.price.toFixed(2)}</p>
-            <Link href={`/c/`}>
-              <button className="btn btn-primary">Order</button>
-            </Link>
+            <div className="card-actions justify-end">
+              <p className="text-lg font-bold leading-10">
+                ${service.price.toFixed(2)}
+              </p>
+              <button
+                className="btn btn-primary"
+                data-shoppy-product={
+                  countryStatus === null || countryStatus === undefined
+                    ? service.ShoppyCode
+                    : countryStatus
+                    ? service.ShoppyCode
+                    : service.ShoppyCodeNoCrypto
+                }
+              >
+                Buy
+              </button>
+              <Link href={`/c/`}>
+                <button className="btn btn-primary">Contact</button>
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-    ));
+      ));
   };
 
   if (loading) return <p>Loading...</p>;
@@ -112,17 +138,19 @@ function ServiceTabsWithCountryCheck() {
   return (
     <div>
       <div className="tabs tabs-lifted">
-        {categories.map(category => (
+        {categories.map((category) => (
           <a
             key={category.id}
-            className={`tab tab-lifted ${activeCategory === category.id ? 'tab-active' : ''}`}
+            className={`tab tab-lifted ${
+              activeCategory === category.id ? "tab-active" : ""
+            }`}
             onClick={() => setActiveCategory(category.id)}
           >
             {category.name}
           </a>
         ))}
       </div>
-      <div className='content-center bg-base-200 card mx-6 mt-5 md:pt-4 px-6'>
+      <div className="content-center bg-base-200 card mx-6 mt-5 md:pt-4 px-6">
         <div className="content-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 bg-base-200 card mx-6 mt-5 md:pt-4 px-6">
           {activeCategory && renderServicesForCategory(activeCategory)}
         </div>
