072b085da927a19850e1aeb7ebf63d4acfdaa79b
mydevify <boussaadilouay9@gmail.com>
2024-08-14 15:11:22 +0100

    changed title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,12 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Whatsapp from "@/components/Layout/Whatsapp";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Discord Keeper",
+  title: "MyHelpDesk",
   description: "Your reliable companion for Discord servers, ensuring your conversations are always preserved. Create and manage tickets seamlessly within your Discord community, with all chat histories securely archived. Even if you leave the server or it's banned, your conversations remain accessible through a convenient login with your Discord account. Keep your discussions safe with DiscordKeeper.",
 };
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
     <html lang="en">
+    <Script src="https://shoppy.gg/api/embed.js"/>
       <body className={inter.className}>
         {children} 
         <Whatsapp/>
