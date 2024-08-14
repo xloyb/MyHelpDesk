598bd827ed461929a6bfa992abf1fb1702689015
mydevify <boussaadilouay9@gmail.com>
2024-08-14 11:18:31 +0100

    updated services Page

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,10 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
-import CheckCountry from "@/components/CheckCountry";
 import Footer from "@/components/Footer";
 import IndexNav from "@/components/index/IndexNav";
-import Services from "@/components/index/Services";
-import Test from "@/components/test";
-import Script from "next/script"; 
+import ServiceTabsWithCountryCheck from "@/components/index/ServiceTabsWithCountryCheck";
+
+import Script from "next/script";
 
 const Store = () => {
   return (
@@ -12,30 +11,7 @@ const Store = () => {
       <Script src="https://shoppy.gg/api/embed.js" strategy="lazyOnload" />
       <div className="bg-base-200">
         <IndexNav />
-        <button
-          style={{
-            background: "#2f2f2f",
-            width: "100%",
-            maxWidth: "200px",
-            marginTop: "-20px"
-          }}
-          data-shoppy-product="2Dh6TwY"
-        >
-          crypto
-        </button>
-
-        <button
-          style={{
-            background: "#2f2f2f",
-            width: "100%",
-            maxWidth: "200px",
-            marginTop: "-20px"
-          }}
-          data-shoppy-product="oORWBGB"
-        >PP</button>
-        <CheckCountry/>
-        {/* <Test/>
-        <Services /> */}
+        <ServiceTabsWithCountryCheck />
         <Footer />
       </div>
     </div>
