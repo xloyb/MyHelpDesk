69e5a9d6a80619564e82c2ec694030bd89e2d804
mydevify <boussaadilouay9@gmail.com>
2024-09-22 01:58:44 +0100

    removed dublicated clerk wrap

--- src/app/ChatLayout/layout.tsx
+++ src/app/ChatLayout/layout.tsx
@@ -2,7 +2,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import Whatsapp from "@/components/Layout/Whatsapp";
 import { Inter } from "next/font/google";
 import Script from "next/script";
-import "./globals.css";
+import "../globals.css";
 import Sidebar from "@/components/Sidebar";
 import styles from "@/app/main.module.css";
 
@@ -11,23 +11,19 @@ import ChatNavbar from "@/components/ChatNavbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Layout1({ children }: { children: React.ReactNode }) {
-    return (
-        <ClerkProvider>
-             <div className={styles.container}>
+  return (
+    <div className={styles.container}>
       <div className={styles.menu}>
         <Sidebar />
       </div>
       <div className={styles.content}>
         <div className="h-screen bg-base-200 overflow-hidden sticky top-0 overflow-y-auto">
           <ChatNavbar />
-          <div className="overflow-y-auto">       
-                <>
-                {children}
-                </>
+          <div className="overflow-y-auto">
+            <>{children}</>
           </div>
         </div>
       </div>
     </div>
-        </ClerkProvider>
-    );
+  );
 }
