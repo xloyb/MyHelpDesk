95ff2e6852d8f4b128ce44e2f4f18101b756fdb0
mydevify <boussaadilouay9@gmail.com>
2024-09-22 03:45:13 +0100

    added a check if the store system is enabled.

--- src/app/admincp/store/page.tsx
+++ src/app/admincp/store/page.tsx
@@ -11,10 +11,26 @@ import ManageCategories from "@/components/admincp/ManageCategories";
 import CountryTable from "@/components/admincp/ManageCountries";
 import Layout1 from "@/app/ChatLayout/layout";
 
+
+type settings = {
+  sitename: string;
+  announcement: string;
+  offer: string;
+  logo: string;
+  theme: string;
+  discordLogs: boolean;
+  exchangeSystem: boolean;
+  storeSystem: boolean;
+  ticketSystem: boolean;
+};
+
+
 const AdminCPRoles = () => {
   const { userId } = useAuth();
   const [loading, setLoading] = useState<boolean>(true);
   const [isAdminMember, setisAdminMember] = useState<boolean>(false);
+  const [Settings, setSettings] = useState<settings | null>(null);
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -27,28 +43,44 @@ const AdminCPRoles = () => {
         setisAdminMember(isAdminMember);
       }
     };
+    const FetchSiteSettings = async () => {
+      try {
+        const response = await fetch('/api/settings');
+        const data: settings = await response.json();
+        setSettings(data);
+      } catch (error) {
+        console.error("Failed to fetch Site Settings:", error);
 
+      }
+    };
+    FetchSiteSettings()
     fetchData();
     checkUserRole();
   }, [userId]);
+
+
+
 
   if (loading) {
     return <div>Loading...</div>;
   }
 
   return (
-   <Layout1>
-    {isAdminMember ? (
-              <>
-              {/* <AuthorizedCountries/> */}
-              <ManageCategories/>
-              <Services />
-              <CountryTable/>
-              </>
-            ) : (
-              <p>You do not have access to this section.</p>
-            )}
-   </Layout1>
+    <Layout1>
+      {isAdminMember ? (
+        <>
+          {/* <AuthorizedCountries/> */}
+          {Settings?.storeSystem ? (<>
+            <ManageCategories />
+            <Services />
+            <CountryTable />
+          </>) : (<>Store is Disabled from the settings, Please Enable it to access this page.</>)}
+
+        </>
+      ) : (
+        <p>You do not have access to this section.</p>
+      )}
+    </Layout1>
   );
 };
 
