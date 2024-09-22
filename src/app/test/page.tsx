6521f787264768b11a6b9f4713f00e1b34eaae69
mydevify <boussaadilouay9@gmail.com>
2024-09-22 01:51:00 +0100

    created a test page

---
+++ src/app/test/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout1 from '../ChatLayout/layout'
+
+const page = () => {
+  return (
+    <div>
+        <Layout1>
+            test
+        </Layout1>
+    </div>
+  )
+}
+
+export default page
