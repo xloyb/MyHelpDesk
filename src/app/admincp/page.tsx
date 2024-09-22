f18e74d466b444ae58869483042ba329710b2b4d
mydevify <boussaadilouay9@gmail.com>
2024-09-22 02:05:33 +0100

    updated admincp homepage

--- src/app/admincp/page.tsx
+++ src/app/admincp/page.tsx
@@ -6,6 +6,7 @@ import ChatNavbar from "@/components/ChatNavbar";
 import { useAuth } from "@clerk/nextjs";
 import styles from "@/app/main.module.css";
 import AdminHome from "@/components/admincp/AdminHome";
+import Layout1 from "../ChatLayout/layout";
 
 const AdminCP = () => {
   const { userId } = useAuth();
@@ -34,25 +35,15 @@ const AdminCP = () => {
   }
 
   return (
-    <div className={styles.container}>
-      <div className={styles.menu}>
-        <Sidebar />
-      </div>
-      <div className={styles.content}>
-        <div className="sticky top-0 z-10 ">
-          <ChatNavbar />
-        </div>
-        <div className="h-screen overflow-y-auto bg-base-200">
-          {isAdminMember ? (
+    <Layout1>
+       {isAdminMember ? (
             <>
               <AdminHome/>
             </>
           ) : (
             <p>You do not have access to this section.</p>
           )}
-        </div>
-      </div>
-    </div>
+    </Layout1>
   );
 };
 
