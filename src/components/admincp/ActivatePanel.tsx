c684737f009db3e4893cf13f700e42d346d4e7a4
mydevify <boussaadilouay9@gmail.com>
2024-09-22 01:41:14 +0100

    added new attributes Settings modal

---
+++ src/components/admincp/ActivatePanel.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AuthorizedCountries = () => {
+  return (
+    <div className="bg-base-100 card mx-6 mt-5 md:pt-4 px-6">
+    <div className="text-xl font-semibold inline-block">Activate</div>
+    <div className='stat-desc text-xs'>You can choose which features to activate in the MyHelpDesk multipurpose panel from this section.</div>
+    <div className="divider mt-2"></div>
+
+  
+  </div>
+  )
+}
+
+export default AuthorizedCountries
