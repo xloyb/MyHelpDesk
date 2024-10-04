09d126a000c8528760448f8735a932ac43d9bd38
mydevify <boussaadilouay9@gmail.com>
2024-10-04 21:59:25 +0100

    added tabes DATA

--- public/DELETE_AFTER_SETUP/Insert_Tables_Info.sql
+++ public/DELETE_AFTER_SETUP/Insert_Tables_Info.sql
@@ -5,13 +5,7 @@ INSERT INTO Role (name, description) VALUES ('Support', 'Support role, support o
 INSERT INTO Role (name, description) VALUES ('Mod', 'Moderator role, moderation duties');
 INSERT INTO Role (name, description) VALUES ('Admin', 'Administrator role, full access');
 
-INSERT INTO user (id, email, avatar, cover, name, userid, roleId) VALUES
-('user_2kHN1lJ3j7KO2jDsVksym31QhoG', 'contactxloy@gmail.com', 'https://images.clerk.dev/oauth_google/img_2kHN1lJ3j7KO2jDsVksym31QhoG', '/img/noCover.png', 'xloy', NULL, 4);
-
-INSERT INTO user (id, email, avatar, cover, name, userid, roleId) VALUES 
-('user_2kHNEANVluU3lEr7KsHABBTrwMH', 'louayeverywhere@gmail.com', 'https://www.gravatar.com/avatar?d=mp', '/img/noCover.png', 'louay', NULL, 1);
-
-INSERT INTO user (id, email, avatar, cover, name, userid, roleId) VALUES 
+INSERT INTO User (id, email, avatar, cover, name, userid, roleId) VALUES 
 ('xLoy_BOT_AmrVYJiRO2kd', 'services@mydevify.com', 'https://www.mydevify.com/assets/index.494ac568.png', '/img/noCover.png', 'Support (Bot)', NULL, 1);
 
 
@@ -21,13 +15,19 @@ INSERT INTO Settings (
   offer, 
   logo, 
   theme, 
-  discordLogs
+  discordLogs,
+ exchangeSystem,
+  storeSystem,
+  ticketSystem
 ) VALUES (
   'MyHelpDesk', 
   'We Love Cats', 
   'Special offer available!', 
   'https://mydevify.com/assets/xLoyMybb.5944be68.png', 
   'dark', 
+  false,
+  false,
+  false,
   false
 );
 
